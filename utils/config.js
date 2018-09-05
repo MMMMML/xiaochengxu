109ab72edaa6b62b676afd7626925fe9b73bb0c0
maling <ling.ma@flashelp.com>
2018-09-05 16:21:49 +0800

    说点饭v地方深V

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 const Config = {
-  baseUrl: 'https://wechat.flashelp.cn/test/api/'  //测试
-  // baseUrl: 'https://wechat.flashelp.cn/api/'  //正式
+  // baseUrl: 'https://wechat.flashelp.cn/test/api/'  //测试
+  baseUrl: 'https://wechat.flashelp.cn/api/'  //正式
 }
 
 export default Config
